17145ef387e48481d78e72db9d14a32f18e09414
Shorab Jamal <shorabjamal@hotmail.ca>
2017-04-05 11:03:58 -0400

    added entity, hbm and cfg files

---
+++ src/main/java/com/sj/entity/Car.java
@@ -0,0 +1,42 @@
+package com.sj.entity;
+
+public class Car {
+
+	private String regNumber;
+	private String ownerName;
+	private String model;
+	private int year;
+
+	public String getRegNumber() {
+		return regNumber;
+	}
+
+	public void setregNumber(String regNumber) {
+		this.regNumber = regNumber;
+	}
+
+	public String getOwnerName() {
+		return ownerName;
+	}
+
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+}
